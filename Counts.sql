91df8c89e95d9165253da2026f478b5ddecdd90e
Coca <cocacore7@gmail.com>
2021-10-29 18:14:24 -0600

    Esto queda como registro

---
+++ Counts.sql
@@ -0,0 +1,17 @@
+select count(*) as TEMPORAL from temporal;
+select count(*) as ANIO_ELECCION from anio_eleccion;
+select count(*) as NOMBRE_ELECCION from nombre_eleccion;
+select count(*) as NOM_ANIO_ELECCION from nom_anio_eleccion;
+
+select count(*) as PAIS from pais;
+select count(*) as REGION from region;
+select count(*) as DEPTO from depto;
+select count(*) as MUNICIPIO from municipio;
+select count(*) as ZONA from zona;
+
+select count(*) as PARTIDO from partido;
+select count(*) as ZONA_PARTIDO from zona_partido;
+
+select count(*) as SEXO from sexo;
+select count(*) as RAZA from raza;
+select count(*) as VOTO from voto;
